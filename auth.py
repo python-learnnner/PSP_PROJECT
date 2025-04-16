b091daf330b96f0d26b7f34305f19c2affcadba2
Bibek Ale <abibek581@gmail.com>
2025-04-16 07:05:08 +0545

    Adding GIT PROJECT.py

--- auth.py
+++ auth.py
@@ -156,3 +156,13 @@
 #     root.geometry("700x600")
 #     app = AuthSystem(root)
 #     root.mainloop()
+def authenticate(username,password):
+    """
+    Authenticate the user with the given username and password.
+    Returns True if credentials are valid, otherwise False."""
+
+    #Examples credentials (replace with a database or secure storage in production)
+    valid_username="admin"
+    valid_password="password"
+
+    return username==valid_username==valid_password
